d35f43be7282cc94317a269e3de1749577dd407d
ZippyNeuron <cloudy.andy@icloud.com>
2026-04-05 22:08:31 +0100

    Added DetectPangramKata

---
+++ src/ZippyNeuron.Kata.Test/DetectPangram/DetectPangramTests.cs
@@ -0,0 +1,19 @@
+﻿using ZippyNeuron.Kata.DetectPangram;
+
+namespace ZippyNeuron.Kata.Test.DetectPangram;
+
+public class DetectPangramTests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [TestCase("The quick brown fox jumps over the lazy dog.", true)]
+        [TestCase("The quick brown fox jumps over the laxy dog.", false)]
+        [TestCase("", false)]
+        [TestCase("!a b 1 c2d3e4f5g6h7iJKLMNopQrSTUVw x y z!", true)]
+        public void SampleTests(string sentence, bool expectedResult)
+        {
+            Assert.That(DetectPangramKata.IsPangram(sentence), Is.EqualTo(expectedResult));
+        }
+    }
+}
---
+++ src/ZippyNeuron.Kata/DetectPangram/DetectPangramKata.cs
@@ -0,0 +1,22 @@
+﻿namespace ZippyNeuron.Kata.DetectPangram;
+
+public class DetectPangramKata
+{
+    public static bool IsPangram(string str)
+    {
+        (int start, int end) = ('a', 'z');
+        var tracker = new int[end - start + 1];
+        var count = 0;
+
+        foreach (var c in str.ToLower())
+        {
+            if (c >= start && c <= end && tracker[c - start] == 0)
+                tracker[c - start] = ++count;
+
+            if (count == tracker.Length)
+                return true;
+        }
+
+        return false;
+    }
+}
