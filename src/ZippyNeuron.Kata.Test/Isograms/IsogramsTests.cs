769615ee76911c1866d646fbd775c14cfaf4029f
ZippyNeuron <cloudy.andy@icloud.com>
2026-04-06 13:28:35 +0100

    Added IsogramsKata

---
+++ src/ZippyNeuron.Kata.Test/Isograms/IsogramsTests.cs
@@ -0,0 +1,24 @@
+﻿using ZippyNeuron.Kata.Isograms;
+
+namespace ZippyNeuron.Kata.Test.Isograms;
+
+public class IsogramsTests
+{
+    private static IEnumerable<TestCaseData> testCases
+    {
+        get
+        {
+            yield return new TestCaseData("Dermatoglyphics").Returns(true);
+            yield return new TestCaseData("isogram").Returns(true);
+            yield return new TestCaseData("moose").Returns(false);
+            yield return new TestCaseData("isIsogram").Returns(false);
+            yield return new TestCaseData("aba").Returns(false);
+            yield return new TestCaseData("moOse").Returns(false);
+            yield return new TestCaseData("thumbscrewjapingly").Returns(true);
+            yield return new TestCaseData("").Returns(true);
+        }
+    }
+
+    [Test, TestCaseSource(nameof(testCases))]
+    public bool Test(string str) => IsogramsKata.IsIsogram(str);
+}
---
+++ src/ZippyNeuron.Kata/Isograms/IsogramsKata.cs
@@ -0,0 +1,24 @@
+﻿namespace ZippyNeuron.Kata.Isograms;
+
+public class IsogramsKata
+{
+    public static bool IsIsogram(string str)
+    {
+        if (string.IsNullOrWhiteSpace(str))
+            return true;
+
+        (int start, int end) = ('a', 'z');
+
+        var tracker = new int[end - start + 1];
+
+        foreach (var c in str.ToLower())
+        {
+            if (c < start && c > end || tracker[c - start] > 0)
+                return false;
+
+            tracker[c - start]++;
+        }
+
+        return true;
+    }
+}
