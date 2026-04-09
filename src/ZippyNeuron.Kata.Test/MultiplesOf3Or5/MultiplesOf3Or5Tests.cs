27edd318ffc06ef7e9a36bbcd76ac6b61c71042a
ZippyNeuron <cloudy.andy@icloud.com>
2026-04-09 20:35:42 +0100

    Added MultiplesOf3Or5

---
+++ src/ZippyNeuron.Kata.Test/MultiplesOf3Or5/MultiplesOf3Or5Tests.cs
@@ -0,0 +1,23 @@
+﻿using ZippyNeuron.Kata.MultiplesOf3Or5;
+
+namespace ZippyNeuron.Kata.Test.MultiplesOf3Or5;
+
+[TestFixture]
+public class MultiplesOf3Or5Tests
+{
+    [Test]
+    public void SampleTests()
+    {
+        Assertion(expected: 23, input: 10);
+        Assertion(expected: 78, input: 20);
+        Assertion(expected: 9168, input: 200);
+        Assertion(expected: 0, input: 0);
+    }
+
+    private static void Assertion(int expected, int input) =>
+      Assert.That(
+        MultiplesOf3Or5Kata.Solution(input),
+        Is.EqualTo(expected),
+        $"Incorrect answer for input={input}"
+      );
+}
---
+++ src/ZippyNeuron.Kata/MultiplesOf3Or5/MultiplesOf3Or5Kata.cs
@@ -0,0 +1,17 @@
+﻿namespace ZippyNeuron.Kata.MultiplesOf3Or5;
+
+public class MultiplesOf3Or5Kata
+{
+    public static int Solution(int value)
+    {
+        if (value < 0)
+            return 0;
+
+        var result = 0;
+
+        for (int i = 1; i < value; i++)
+            result += i % 3 == 0 ? i : i % 5 == 0 ? i : 0;
+
+        return result;
+    }
+}
