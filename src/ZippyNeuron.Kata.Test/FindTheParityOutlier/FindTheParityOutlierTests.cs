e48f29ea05b9efa71af2686d84f6a2c442291ace
ZippyNeuron <cloudy.andy@icloud.com>
2026-04-11 15:21:16 +0100

    Added FindTheParityOutlier

---
+++ src/ZippyNeuron.Kata.Test/FindTheParityOutlier/FindTheParityOutlierTests.cs
@@ -0,0 +1,84 @@
+﻿using ZippyNeuron.Kata.FindTheParityOutlier;
+
+namespace ZippyNeuron.Kata.Test.FindTheParityOutlier;
+
+[TestFixture]
+internal class FindTheParityOutlierTests
+{
+    private static void DoTest(int expected, int[] input)
+    {
+        String message = "for array { " + string.Join(", ", input) + " }\n";
+        int actual = FindTheParityOutlierKata.Find(input);
+        Assert.That(actual, Is.EqualTo(expected), message);
+    }
+
+    [Test]
+    [Order(1)]
+    public static void Test1()
+    {
+        DoTest(3, new int[] { 2, 6, 8, -10, 3 });
+    }
+
+    [Test]
+    [Order(2)]
+    public static void Test2()
+    {
+        DoTest(206847684, new int[] { 206847684, 1056521, 7, 17, 1901, 21104421, 7, 1, 35521, 1, 7781 });
+    }
+
+    [Test]
+    [Order(3)]
+    public static void Test3()
+    {
+        DoTest(0, new int[] { int.MaxValue, 0, 1 });
+    }
+
+    [Test]
+    [Order(4)]
+    public static void Test4()
+    {
+        DoTest(-5, new int[] { -5, 6, 6 });
+    }
+
+    [Test]
+    [Order(1)]
+    public static void Test5()
+    {
+        DoTest(-1, new int[] { });
+    }
+
+    [Test]
+    [Order(1)]
+    public static void Test6()
+    {
+        DoTest(-1, new int[] { 1 });
+    }
+
+    [Test]
+    [Order(1)]
+    public static void Test7()
+    {
+        DoTest(-1, new int[] { 1, 2 });
+    }
+
+    [Test]
+    [Order(1)]
+    public static void Test8()
+    {
+        DoTest(2, new int[] { 1, 2, 3 });
+    }
+
+    [Test]
+    [Order(1)]
+    public static void Test9()
+    {
+        DoTest(-1, new int[] { 1, 5, 3 });
+    }
+
+    [Test]
+    [Order(1)]
+    public static void Test10()
+    {
+        DoTest(-1, new int[] { 2, 4, 8 });
+    }
+}
---
+++ src/ZippyNeuron.Kata/FindTheParityOutlier/FindTheParityOutlierKata.cs
@@ -0,0 +1,33 @@
+﻿namespace ZippyNeuron.Kata.FindTheParityOutlier;
+
+public class FindTheParityOutlierKata
+{
+    public static int Find(int[] integers)
+    {
+        (int Index, int Number) odd = (-1, 0);
+        (int Index, int Number) even = (-1, 0);
+        (int Index, int Number) other = (-1, 0);
+
+        for (int i = 0; i < integers.Length; i++)
+        {
+            odd = odd.Index == -1 && integers[i] % 2 != 0
+                ? (i, integers[i])
+                : odd;
+
+            even = even.Index == -1 && integers[i] % 2 == 0
+                ? (i, integers[i])
+                : even;
+
+            other = i != odd.Index && i != even.Index
+                ? (i, integers[i])
+                : other;
+
+            if (odd.Index != -1 && even.Index != -1 && other.Index != -1)
+                return other.Number % 2 != 0
+                    ? even.Number
+                    : odd.Number;
+        }
+
+        return -1;
+    }
+}
