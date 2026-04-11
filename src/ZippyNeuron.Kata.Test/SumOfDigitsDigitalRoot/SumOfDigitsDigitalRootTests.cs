90f46cc0bf21084cf889dac48b3c735f5df559e6
ZippyNeuron <cloudy.andy@icloud.com>
2026-04-11 12:38:20 +0100

    Added SumOfDigitsDigitalRoot

---
+++ src/ZippyNeuron.Kata.Test/SumOfDigitsDigitalRoot/SumOfDigitsDigitalRootTests.cs
@@ -0,0 +1,21 @@
+﻿using ZippyNeuron.Kata.SumOfDigitsDigitalRoot;
+
+namespace ZippyNeuron.Kata.Test.SumOfDigitsDigitalRoot;
+
+[TestFixture]
+public class SumOfDigitsDigitalRootTests
+{
+    [TestCase(0, ExpectedResult = 0)]
+    [TestCase(10, ExpectedResult = 1)]
+    [TestCase(16, ExpectedResult = 7)]
+    [TestCase(195, ExpectedResult = 6)]
+    [TestCase(992, ExpectedResult = 2)]
+    [TestCase(167346, ExpectedResult = 9)]
+    [TestCase(999999999999, ExpectedResult = 9)]
+    [TestCase(1234567890, ExpectedResult = 9)]
+    [Order(1)]
+    public int Tests(long n)
+    {
+        return SumOfDigitsDigitalRootKata.DigitalRoot(n);
+    }
+}
---
+++ src/ZippyNeuron.Kata/SumOfDigitsDigitalRoot/SumOfDigitsDigitalRootKata.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.WebSockets;
+
+namespace ZippyNeuron.Kata.SumOfDigitsDigitalRoot;
+
+public class SumOfDigitsDigitalRootKata
+{
+    public static int DigitalRoot(long n)
+    {
+        var total = 0;
+
+        foreach (char number in n.ToString())
+            total += number - '0';
+
+        if (total > 9)
+            return DigitalRoot(total);
+
+        return total;
+    }
+}
