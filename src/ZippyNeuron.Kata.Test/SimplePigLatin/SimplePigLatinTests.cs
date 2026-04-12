642029873ce195e7c47caf43b240752d78be8467
ZippyNeuron <cloudy.andy@icloud.com>
2026-04-12 13:55:08 +0100

    Added SimplePigLatin

---
+++ src/ZippyNeuron.Kata.Test/SimplePigLatin/SimplePigLatinTests.cs
@@ -0,0 +1,18 @@
+﻿using ZippyNeuron.Kata.SimplePigLatin;
+
+namespace ZippyNeuron.Kata.Test.SimplePigLatin;
+
+[TestFixture]
+public class SimplePigLatinTests
+{
+    [Test]
+    public void KataTests()
+    {
+        Assert.That(SimplePigLatinKata.PigIt("Hello world !"), Is.EqualTo("elloHay orldway !"));
+        Assert.That(SimplePigLatinKata.PigIt("P"), Is.EqualTo("Pay"));
+        Assert.That(SimplePigLatinKata.PigIt("Pay"), Is.EqualTo("ayPay"));
+        Assert.That(SimplePigLatinKata.PigIt("A small collection of a e i o u"), Is.EqualTo("Aay mallsay ollectioncay foay aay eay iay oay uay"));
+        Assert.That(SimplePigLatinKata.PigIt("Pig latin is cool"), Is.EqualTo("igPay atinlay siay oolcay"));
+        Assert.That(SimplePigLatinKata.PigIt("This is my string"), Is.EqualTo("hisTay siay ymay tringsay"));
+    }
+}
---
+++ src/ZippyNeuron.Kata/SimplePigLatin/SimplePigLatinKata.cs
@@ -0,0 +1,15 @@
+﻿namespace ZippyNeuron.Kata.SimplePigLatin;
+
+public class SimplePigLatinKata
+{
+    public static string PigIt(string str)
+    {
+        var words = str.Split(' ');
+
+        for (int i = 0; i < words.Length; i++)
+            if (words[i].Length > 0 && char.IsLetter(words[i][0]))
+                words[i] = $"{words[i][1..]}{words[i][..1]}ay";
+
+        return String.Join(' ', words);
+    }
+}
