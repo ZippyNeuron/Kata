16607acfcdc31eb1c21a340f4ec5fa2bbac38eb0
ZippyNeuron <cloudy.andy@icloud.com>
2026-04-12 13:06:03 +0100

    Added RgbToHexConversion

---
+++ src/ZippyNeuron.Kata.Test/RgbToHexConversion/RgbToHexConversionTests.cs
@@ -0,0 +1,21 @@
+﻿using ZippyNeuron.Kata.RgbToHexConversion;
+
+namespace ZippyNeuron.Kata.Test.RgbToHexConversion;
+
+[TestFixture]
+public class RgbToHexConversionTests
+{
+    [Test]
+    [Order(1)]
+    public void FixedTests()
+    {
+        Assert.That(RgbToHexConversionKata.Rgb(127, 127, 127), Is.EqualTo("7F7F7F"));
+        Assert.That(RgbToHexConversionKata.Rgb(255, 255, 255), Is.EqualTo("FFFFFF"));
+        Assert.That(RgbToHexConversionKata.Rgb(255, 255, 300), Is.EqualTo("FFFFFF"));
+        Assert.That(RgbToHexConversionKata.Rgb(0, 0, 0), Is.EqualTo("000000"));
+        Assert.That(RgbToHexConversionKata.Rgb(148, 0, 211), Is.EqualTo("9400D3"));
+        Assert.That(RgbToHexConversionKata.Rgb(148, -20, 211), Is.EqualTo("9400D3"), "Handle negative numbers.");
+        Assert.That(RgbToHexConversionKata.Rgb(144, 195, 212), Is.EqualTo("90C3D4"));
+        Assert.That(RgbToHexConversionKata.Rgb(212, 53, 12), Is.EqualTo("D4350C"), "Consider single hex digit numbers.");
+    }
+}
---
+++ src/ZippyNeuron.Kata/RgbToHexConversion/RgbToHexConversionKata.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace ZippyNeuron.Kata.RgbToHexConversion;
+
+public class RgbToHexConversionKata
+{
+    public static string Rgb(int r, int g, int b)
+    {
+        var clamp = (int v) => 
+            v < 0 ? 0 : v > 255 ? 255 : v;
+
+        return $"{clamp(r):X2}{clamp(g):X2}{clamp(b):X2}";
+    }
+}
