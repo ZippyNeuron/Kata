b3f31f1cd8ffdd6690af295ec42c7d936f3732a8
ZippyNeuron <cloudy.andy@icloud.com>
2026-04-13 18:42:44 +0100

    Added HumanReadableTime

---
+++ src/ZippyNeuron.Kata.Test/HumanReadableTime/HumanReadableTimeTests.cs
@@ -0,0 +1,28 @@
+﻿using ZippyNeuron.Kata.HumanReadableTime;
+
+namespace ZippyNeuron.Kata.Test.HumanReadableTime;
+
+[TestFixture]
+public class HumanReadableTimeTests
+{
+    private void DoTest(int seconds, String expected)
+    {
+        String actual = HumanReadableTimeKata.GetReadableTime(seconds);
+        Assert.That(actual, Is.EqualTo(expected), "for " + seconds + " seconds");
+    }
+
+    [Test]
+    public void SampleTests()
+    {
+        DoTest(0, "00:00:00");
+        DoTest(59, "00:00:59");
+        DoTest(60, "00:01:00");
+        DoTest(90, "00:01:30");
+        DoTest(3599, "00:59:59");
+        DoTest(3600, "01:00:00");
+        DoTest(45296, "12:34:56");
+        DoTest(86399, "23:59:59");
+        DoTest(86400, "24:00:00");
+        DoTest(359999, "99:59:59");
+    }
+}
---
+++ src/ZippyNeuron.Kata/HumanReadableTime/HumanReadableTimeKata.cs
@@ -0,0 +1,13 @@
+﻿namespace ZippyNeuron.Kata.HumanReadableTime;
+
+public class HumanReadableTimeKata
+{
+    public static string GetReadableTime(int seconds)
+    {
+        var h = seconds / 3600;
+        var m = seconds % 3600 / 60;
+        var s = seconds % 60;
+
+        return $"{h:D2}:{m:D2}:{s:D2}";
+    }
+}
