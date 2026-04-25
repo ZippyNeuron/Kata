2e757516079a4fab4e9e35cb384d2a994303b5d1
ZippyNeuron <cloudy.andy@icloud.com>
2026-04-25 12:09:19 +0100

    Added FindTheMissingLetter

---
+++ src/ZippyNeuron.Kata.Test/FindTheMissingLetter/FindTheMissingLetterTests.cs
@@ -0,0 +1,14 @@
+﻿using ZippyNeuron.Kata.FindTheMissingLetter;
+
+namespace ZippyNeuron.Kata.Test.FindTheMissingLetter;
+
+[TestFixture]
+public class FindTheMissingLetterTests
+{
+    [Test]
+    public void ExampleTests()
+    {
+        Assert.That(FindTheMissingLetterKata.FindMissingLetter(new[] { 'a', 'b', 'c', 'd', 'f' }), Is.EqualTo('e'));
+        Assert.That(FindTheMissingLetterKata.FindMissingLetter(new[] { 'O', 'Q', 'R', 'S' }), Is.EqualTo('P'));
+    }
+}
---
+++ src/ZippyNeuron.Kata/FindTheMissingLetter/FindTheMissingLetterKata.cs
@@ -0,0 +1,15 @@
+﻿namespace ZippyNeuron.Kata.FindTheMissingLetter;
+
+public class FindTheMissingLetterKata
+{
+    public static char FindMissingLetter(char[] array)
+    {
+        for (int i = 0; i < array.Length - 1; i++)
+        {
+            if (array[i + 1] - array[i] > 1)
+                return (char)(array[i] + 1);
+        }
+
+        return ' ';
+    }
+}
