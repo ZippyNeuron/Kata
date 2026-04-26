319c42c3c77d2de3a181ba82aec551997cbd46d0
ZippyNeuron <cloudy.andy@icloud.com>
2026-04-26 16:31:18 +0100

    Added WhichAreIn

---
+++ src/ZippyNeuron.Kata.Test/WhichAreIn/WhichAreInTests.cs
@@ -0,0 +1,34 @@
+﻿using ZippyNeuron.Kata.WhichAreIn;
+
+namespace ZippyNeuron.Kata.Test.WhichAreIn;
+
+[TestFixture]
+public class WhichAreInTests
+{
+    [Test]
+    public void Test1()
+    {
+        string[] a1 = { "arp", "live", "strong" };
+        string[] a2 = { "lively", "alive", "harp", "sharp", "armstrong" };
+        string[] r = { "arp", "live", "strong" };
+        Assert.That(WhichAreInKata.inArray(a1, a2), Is.EqualTo(r));
+    }
+
+    [Test]
+    public void Test2()
+    {
+        string[] a1 = { };
+        string[] a2 = { "lively", "alive", "harp", "sharp", "armstrong" };
+        string[] r =  { };
+        Assert.That(WhichAreInKata.inArray(a1, a2), Is.EqualTo(r));
+    }
+
+    [Test]
+    public void Test3()
+    {
+        string[] a1 = { "wibble" };
+        string[] a2 = { "lively", "alive", "harp", "sharp", "armstrong" };
+        string[] r = { };
+        Assert.That(WhichAreInKata.inArray(a1, a2), Is.EqualTo(r));
+    }
+}
---
+++ src/ZippyNeuron.Kata/WhichAreIn/WhichAreInKata.cs
@@ -0,0 +1,25 @@
+﻿namespace ZippyNeuron.Kata.WhichAreIn;
+
+public class WhichAreInKata
+{
+    public static string[] inArray(string[] array1, string[] array2)
+    {
+        var results = new HashSet<string>();
+
+        Array.Sort(array1);
+
+        foreach(var a1 in array1)
+        {
+            foreach (var a2 in array2)
+            {
+                if (a2.IndexOf(a1) > -1)
+                {
+                    results.Add(a1);
+                    break;
+                }
+            }
+        }
+
+        return [..results];
+    }
+}
