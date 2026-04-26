8707a4339611b729fe76b6b930d965b9b59a03d5
ZippyNeuron <cloudy.andy@icloud.com>
2026-04-26 10:55:44 +0100

    Added TitleCase

---
+++ src/ZippyNeuron.Kata.Test/TitleCase/TitleCaseTests.cs
@@ -0,0 +1,38 @@
+﻿using ZippyNeuron.Kata.TitleCase;
+
+namespace ZippyNeuron.Kata.Test.TitleCase;
+
+[TestFixture]
+public class TitleCaseTests
+{
+    [TestCase("a clash of KINGS", "a an the of", "A Clash of Kings")]
+    [TestCase("THE WIND IN THE WILLOWS", "The In", "The Wind in the Willows")]
+    [Order(1)]
+    public void MyTest(string sampleTitle, string sampleMinorWords, string expected)
+    {
+        Assert.That(TitleCaseKata.TitleCase(sampleTitle, sampleMinorWords), Is.EqualTo(expected));
+    }
+
+    [Test, Order(2)]
+    public void MyTest2()
+    {
+        Assert.That(TitleCaseKata.TitleCase(""), Is.EqualTo(""));
+    }
+
+    [Test, Order(3)]
+    public void MyTest3()
+    {
+        Assert.That(TitleCaseKata.TitleCase("the quick brown fox"), Is.EqualTo("The Quick Brown Fox"));
+    }
+
+    [TestCase("", "", "")]
+    [TestCase("a", "a", "A")]
+    [TestCase("a fish", "a", "A Fish")]
+    [TestCase("a fish tale", "a tale", "A Fish tale")]
+    [TestCase("First a of in","an often into","First A Of In")]
+    [Order(4)]
+    public void MyExtraTest(string sampleTitle, string sampleMinorWords, string expected)
+    {
+        Assert.That(TitleCaseKata.TitleCase(sampleTitle, sampleMinorWords), Is.EqualTo(expected));
+    }
+}
---
+++ src/ZippyNeuron.Kata/TitleCase/TitleCaseKata.cs
@@ -0,0 +1,25 @@
+﻿namespace ZippyNeuron.Kata.TitleCase;
+
+public class TitleCaseKata
+{
+    public static string TitleCase(string title, string minorWords = "")
+    {
+        var delimiter = ' ';
+        var words = title.Split(delimiter);
+        var minors = minorWords.ToLowerInvariant().Split(delimiter);
+        var outputs = new string[words.Length];
+
+        for(int i = 0; i < words.Length; i++)
+        {
+            var isMinor = i != 0 && minors.Contains(words[i].ToLower());
+
+            outputs[i] = isMinor
+                ? words[i].ToLowerInvariant()
+                : words[i].Length > 1
+                    ? $"{words[i][..1].ToUpperInvariant()}{words[i][1..].ToLowerInvariant()}"
+                    : words[i].ToUpperInvariant();
+        }
+
+        return string.Join(delimiter, outputs);
+    }
+}
