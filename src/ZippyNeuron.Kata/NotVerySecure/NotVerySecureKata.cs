a602d85b7cb58d8108c2aebceb69e58697cbb920
ZippyNeuron <cloudy.andy@icloud.com>
2026-05-05 19:45:24 +0100

    Added NotVerySecure

---
+++ src/ZippyNeuron.Kata/NotVerySecure/NotVerySecureKata.cs
@@ -0,0 +1,21 @@
+﻿namespace ZippyNeuron.Kata.NotVerySecure;
+
+public class NotVerySecureKata
+{
+    public static bool Alphanumeric(string str)
+    {
+        if (str.Length == 0)
+            return false;
+
+        var allowed = new HashSet<char>(
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890");
+
+        foreach (char c in str)
+        {
+            if (!allowed.Contains(c))
+                return false;
+        }
+
+        return true;
+    }
+}
