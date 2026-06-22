cec83abe0e5081e819ae8b048f66845e2e2e2278
ZippyNeuron <cloudy.andy@icloud.com>
2026-06-22 13:01:46 +0100

    Added SumTheTree

---
+++ src/ZippyNeuron.Kata.Test/SumTheTree/SumTheTreeTests.cs
@@ -0,0 +1,20 @@
+﻿using ZippyNeuron.Kata.SumTheTree;
+
+namespace ZippyNeuron.Kata.Test.SumTheTree;
+
+[TestFixture]
+public class SumTheTreeTests
+{
+    private static IEnumerable<TestCaseData> testCases
+    {
+        get
+        {
+            yield return new TestCaseData(new Node(10, new Node(1), new Node(2))).Returns(13).SetDescription("Simple Test");
+            yield return new TestCaseData(new Node(11, new Node(0), new Node(0, null, new Node(1)))).Returns(12).SetDescription("Handles unbalanced trees");
+        }
+    }
+
+    [Test, TestCaseSource("testCases")]
+    public int Test(Node root) =>
+      SumTheTreeKata.SumTree(root);
+}
---
+++ src/ZippyNeuron.Kata/SumTheTree/SumTheTreeKata.cs
@@ -0,0 +1,20 @@
+﻿namespace ZippyNeuron.Kata.SumTheTree;
+public class Node
+{
+    public int Value;
+    public Node Left;
+    public Node Right;
+
+    public Node(int value, Node left = null, Node right = null)
+    {
+        Value = value;
+        Left = left;
+        Right = right;
+    }
+}
+
+public class SumTheTreeKata
+{
+    public static int SumTree(Node root) =>
+        (root != null ? root.Value + SumTree(root!.Left) + SumTree(root!.Right) : 0);
+}
