95c4d2eb974c959e399b022418171cc8aa4094fb
ZippyNeuron <cloudy.andy@icloud.com>
2026-06-22 12:44:09 +0100

    Added AddingBigNumbers

---
+++ src/ZippyNeuron.Kata.Test/AddingBigNumbers/AddingBigNumbersTests.cs
@@ -0,0 +1,14 @@
+﻿using ZippyNeuron.Kata.AddingBigNumbers;
+
+namespace ZippyNeuron.Kata.Test.AddingBigNumbers;
+
+public class AddingBigNumbersTests
+{
+    [Test]
+    public void ASimpleKataTest()
+    {
+        Assert.That(AddingBigNumbersKata.Add("91", "19"), Is.EqualTo("110"));
+        Assert.That(AddingBigNumbersKata.Add("123456789", "987654322"), Is.EqualTo("1111111111"));
+        Assert.That(AddingBigNumbersKata.Add("999999999", "1"), Is.EqualTo("1000000000"));
+    }
+}
---
+++ src/ZippyNeuron.Kata/AddingBigNumbers/AddingBigNumbersKata.cs
@@ -0,0 +1,16 @@
+﻿using System.Numerics;
+
+namespace ZippyNeuron.Kata.AddingBigNumbers;
+
+public class AddingBigNumbersKata
+{
+    public static string Add(string a, string b)
+    {
+        if (!BigInteger.TryParse(a, out var aValue))
+            return "0";
+        if (!BigInteger.TryParse(b, out var bValue))
+            return "0";
+
+        return (aValue + bValue).ToString();
+    }
+}
