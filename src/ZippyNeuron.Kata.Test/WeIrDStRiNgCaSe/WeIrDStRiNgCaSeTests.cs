e630d32b84d58cd5e18f01d9b8815445abc5d75a
ZippyNeuron <cloudy.andy@icloud.com>
2026-06-23 16:26:06 +0100

    Added WeIrDStRiNgCaSe

---
+++ src/ZippyNeuron.Kata.Test/WeIrDStRiNgCaSe/WeIrDStRiNgCaSeTests.cs
@@ -0,0 +1,21 @@
+﻿using ZippyNeuron.Kata.WeIrDStRiNgCaSe;
+
+namespace ZippyNeuron.Kata.Test.WeIrDStRiNgCaSe;
+
+[TestFixture]
+internal class WeIrDStRiNgCaSeTests
+{
+    private static void DoTest(String input, String expected)
+    {
+        String actual = WeIrDStRiNgCaSeKata.ToWeirdCase(input);
+        Assert.That(actual, Is.EqualTo(expected), "for input: \"" + input + "\"\n");
+    }
+
+    [Test]
+    public static void ShouldWorkForSomeExamples()
+    {
+        DoTest("This", "ThIs");
+        DoTest("is", "Is");
+        DoTest("This is a test", "ThIs Is A TeSt");
+    }
+}
---
+++ src/ZippyNeuron.Kata/WeIrDStRiNgCaSe/WeIrDStRiNgCaSeKata.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace ZippyNeuron.Kata.WeIrDStRiNgCaSe;
+
+// https://www.codewars.com/kata/52b757663a95b11b3d00062d/train/csharp
+
+public class WeIrDStRiNgCaSeKata
+{
+    public static string ToWeirdCase(string s)
+    {
+        if (string.IsNullOrWhiteSpace(s))
+            return string.Empty;
+
+        var result = new char[s.Length];
+        var index = 0;
+
+        for(int i = 0; i < s.Length; i++)
+        {
+            result[i] = index % 2 != 0 ? Char.ToLowerInvariant(s[i]) : Char.ToUpperInvariant(s[i]);
+
+            index++;
+
+            if (s[i] == ' ')
+                index = 0;
+        }
+
+        return new string(result);
+    }
+}
