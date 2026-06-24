623d47777baafe080624c4d98ffc6095874e72a9
ZippyNeuron <cloudy.andy@icloud.com>
2026-06-24 19:32:18 +0100

    Added MovingZerosToTheEnd

---
+++ src/ZippyNeuron.Kata.Test/MovingZerosToTheEnd/MovingZerosToTheEndTests.cs
@@ -0,0 +1,15 @@
+﻿using ZippyNeuron.Kata.MovingZerosToTheEnd;
+
+namespace ZippyNeuron.Kata.Test.MovingZerosToTheEnd;
+
+[TestFixture]
+public class MovingZerosToTheEndTests
+{
+    [Test]
+    [Order(1)]
+    public void Test()
+    {
+        int[] expected = new int[] { 1, 2, 1, 1, 3, 1, 0, 0, 0, 0 };
+        Assert.That(MovingZerosToTheEndKata.MoveZeroes(new int[] { 1, 2, 0, 1, 0, 1, 0, 3, 0, 1 }), Is.EqualTo(expected));
+    }
+}
---
+++ src/ZippyNeuron.Kata/MovingZerosToTheEnd/MovingZerosToTheEndKata.cs
@@ -0,0 +1,21 @@
+﻿namespace ZippyNeuron.Kata.MovingZerosToTheEnd;
+
+public class MovingZerosToTheEndKata
+{
+    public static int[] MoveZeroes(int[] arr)
+    {
+        var result = new int[arr.Length];
+        var count = 0;
+
+        for(int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] != 0)
+            {
+                result[count] = arr[i];
+                count++;
+            }
+        }
+
+        return result;
+    }
+}
