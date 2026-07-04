bea2852a554fed89eb2026f106e80833df7e9404
ZippyNeuron <cloudy.andy@icloud.com>
2026-07-04 16:24:59 +0100

    Update to make-uppercase

--- src/c/make-uppercase.c
+++ src/c/make-uppercase.c
@@ -3,21 +3,18 @@
 #include <string.h>
 #include <ctype.h>
 
-// https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7/train/c
-
 const char * to_uppercase(char *value);
 
 int main() {   
     char value[] = "abcABC1234@@@@###";
-
     printf("%s\n", to_uppercase(value));
-
     return 0;
 }
 
-const char * to_uppercase(char *value) {
-    for (int i = 0; i < strlen(value); i++)
-        value[i] = toupper(value[i]);
+const char * to_uppercase(char *string) {
+    // https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7/train/c
+    for (int i = 0; i < strlen(string); i++)
+        string[i] = toupper(string[i]);
 
-    return value;
+    return string;
 }
