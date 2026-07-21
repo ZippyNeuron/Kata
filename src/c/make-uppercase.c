2aa046740193b1b53ea84a1361fc3822563cd9f9
ZippyNeuron <cloudy.andy@icloud.com>
2026-07-21 13:00:04 +0100

    Some minor cleanup and added new launch.json

--- src/c/make-uppercase.c
+++ src/c/make-uppercase.c
@@ -12,7 +12,6 @@ int main() {
 }
 
 const char * to_uppercase(char *string) {
-    // https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7/train/c
     for (int i = 0; i < strlen(string); i++)
         string[i] = toupper(string[i]);
 
